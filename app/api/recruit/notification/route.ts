31c26148da3ecb7676d501d83bdf7d8e3df4fdaa
LeeBonHoon <80507816+LeeBonHoon1@users.noreply.github.com>
2025-05-07 02:23:49 +0900

    [ FEAT ] query 수정 및 schame 동기화

--- app/api/recruit/notification/route.ts
+++ app/api/recruit/notification/route.ts
@@ -29,7 +29,7 @@ export async function POST(request: NextRequest) {
       data: { email },
     });
 
-    const isAlreadyInWaitlist = await prisma.recruitWaitlist.findUnique({
+    const isAlreadyInWaitlist = await prisma.recruitWaitlist.findFirst({
       where: { userId },
     });
 
